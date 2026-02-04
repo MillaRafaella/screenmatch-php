7608349c9b48c158c8a6326b304c8169c15d6169
Milla <mrrborges84@gmail.com>
2026-02-03 21:58:51 -0300

    Commit 030226

--- src/Modelo/Filme.php
+++ src/Modelo/Filme.php
@@ -2,8 +2,31 @@
 
 class Filme 
 {
-    public string $nome;
-    public int $anoLancamento;
-    public string $genero;
-    public float $nota;
+    private string $nome = 'Nome Padrão';
+    private int $anoLancamento = 2024;
+    private string $genero = 'ação';
+    private array $notas = [];
+
+    public function avalia(float $nota): void
+    {
+        $this->notas[] = $nota;
+    }
+
+    public function media(): float
+    {
+        $somaNotas = array_sum($this->notas);
+        $quantidadeNotas = count($this->notas);
+        
+        return $somaNotas / $quantidadeNotas;
+    }
+
+    public function anoLancamento(): int
+    {
+        return $this->anoLancamento;
+    }
+
+    public function defineAnoLancamento(int $anoLancamento): void
+    {
+        $this->anoLancamento = $anoLancamento;
+    }
 }
